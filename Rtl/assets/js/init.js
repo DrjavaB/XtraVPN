6b2ca0adcdb33e023a343212f7dbf5c795d338bf
Dr JavaB <maghami.rad1@gmail.com>
2020-05-23 13:19:45 +0430

    blog 2 web

--- Rtl/assets/js/init.js
+++ Rtl/assets/js/init.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
 	AOS.init();
-	$(".owl-carousel").owlCarousel({
-		rtl: true,
+	$(".owl-main").owlCarousel({
+		rtl:                true,
 		nav:                true,
 		dots:               true,
 		autoplay:           true,
 		autoplayTimeout:    5000,
+		smartSpeed:         2000,
 		autoplayHoverPause: true,
 		loop:               true,
-		slideTransition:    'ease-in-out',
+		slideTransition:    'cubic-bezier(0.32,-0.3, 0.7, 1.26)',
 		responsive:         {
 			0:    {
 				items:    1,
@@ -29,6 +30,39 @@ $(document).ready(function () {
 			1200: {
 				items:    3,
 				dotsEach: 3,
+			}
+		}
+	});
+	$(".owl-blog").owlCarousel({
+		rtl:                true,
+		nav:                true,
+		dots:               true,
+		autoplay:           true,
+		autoplayTimeout:    4000,
+		smartSpeed:         2000,
+		autoplayHoverPause: true,
+		loop:               true,
+		slideTransition:    'cubic-bezier(0.32,-0.3, 0.7, 1.26)',
+		responsive:         {
+			0:    {
+				items:    1,
+				dotsEach: 1,
+			},
+			576:  {
+				items:    1,
+				dotsEach: 1,
+			},
+			768:  {
+				items:    2,
+				dotsEach: 2,
+			},
+			992:  {
+				items:    2,
+				dotsEach: 2,
+			},
+			1200: {
+				items:    2,
+				dotsEach: 2,
 			}
 		}
 	});
@@ -59,6 +93,21 @@ $(document).ready(function () {
 		}
 	});
 	$('#preloader').hide();
+	var left  = document.getElementsByClassName('article-left')[0],
+	    right = document.getElementsByClassName('article-right')[0],
+	    prev  = document.getElementsByClassName('article-preview');
+	left.addEventListener('mouseover', function () {
+		prev[0].style.animation = 'anim-left 600ms 300ms ease-in-out forwards';
+	});
+	left.addEventListener('mouseout', function () {
+		prev[0].style.animation = 'anim-left-not 600ms 300ms ease-in-out backwards';
+	});
+	right.addEventListener('mouseover', function () {
+		prev[1].style.animation = 'anim-right 600ms 300ms ease-in-out forwards';
+	});
+	right.addEventListener('mouseout', function () {
+		prev[1].style.animation = 'anim-right-not 600ms 300ms ease-in-out backwards';
+	});
 });
 
 function expand() {
